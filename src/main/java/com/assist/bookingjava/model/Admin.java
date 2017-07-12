239f27756e6fc1326ab0baffb6e197b9063fdcb8
tatarudimitrie <intern.dimitrie.tataru@assist.ro>
2017-07-12 16:16:55 +0300

    Database init

---
+++ src/main/java/com/assist/bookingjava/model/Admin.java
@@ -0,0 +1,7 @@
+package com.assist.bookingjava.model;
+
+/**
+ * Created by myt on 12.07.2017.
+ */
+public class Admin {
+}
---
+++ src/main/java/com/assist/bookingjava/repositories/AdminRepository.java
@@ -0,0 +1,7 @@
+package com.assist.bookingjava.repositories;
+
+/**
+ * Created by myt on 12.07.2017.
+ */
+public class AdminRepository {
+}
