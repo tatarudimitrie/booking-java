6bfbc029bdc159cc0724da20f27487239843ef50
LeonteI <intern.andrei.leonte@assist.ro>
2017-07-19 14:12:09 +0300

    a

---
+++ src/main/java/com/assist/bookingjava/Security/Webconfig.java
@@ -0,0 +1,21 @@
+package com.assist.bookingjava.Security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+/**
+ * Created by root on 14.07.2017.
+ */
+@Configuration
+@EnableWebSecurity
+public class Webconfig extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedMethods("HEAD", "GET", "PUT", "POST", "DELETE", "PATCH");
+    }
+
+}
