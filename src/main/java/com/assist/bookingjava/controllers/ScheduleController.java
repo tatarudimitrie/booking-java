07d63f8c6142a9eadbee84484f3626e979275baa
LeonteI <intern.andrei.leonte@assist.ro>
2017-07-20 11:58:38 +0300

    Map

--- src/main/java/com/assist/bookingjava/controllers/ScheduleController.java
+++ src/main/java/com/assist/bookingjava/controllers/ScheduleController.java
@@ -1,7 +1,9 @@
 package com.assist.bookingjava.controllers;
 
 import com.assist.bookingjava.model.Schedule;
+import com.assist.bookingjava.model.Service;
 import com.assist.bookingjava.services.ScheduleService;
+import org.hibernate.boot.jaxb.SourceType;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -10,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Map;
 
 @RestController
 public class ScheduleController {
@@ -27,8 +30,10 @@ public class ScheduleController {
         return scheduleService.addScheduleAll(schedule);
     }
 
-    @RequestMapping(method = RequestMethod.PUT, value = "/schedule/edit")
-    public ResponseEntity<String> editSchedule(@RequestBody List<Schedule> schedule){
-        return scheduleService.addScheduleAll(schedule);
+    @RequestMapping(method = RequestMethod.POST, value = "/schedule/edit")
+    public ResponseEntity<String> editSchedule(@RequestBody Map<String,List<Object>> schedule){
+        System.out.println(schedule.toString());
+        return ResponseEntity.ok("ASd");
+        //return scheduleService.addScheduleAll(schedule);
     }
 }
